14e651b22e866cd926fcd20f765ea35e679ff64c
Ryan Breen <ryan@breen.xyz>
2024-02-07 23:07:29 -0500

    Change from using tx hash to outputs.

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,11 @@
+use std::str::FromStr;
+
 use askama::Template;
 use axum::{Form, Router};
-use bitcoin::{Network, Transaction};
+use bitcoin::{
+    absolute::LockTime, transaction::Version, Address, Amount, Network, Sequence, Transaction,
+    TxIn, TxOut,
+};
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 
@@ -9,7 +14,7 @@ use crate::ctv;
 pub async fn server() -> anyhow::Result<()> {
     let app = Router::new()
         .route("/", axum::routing::get(index))
-        .route("/ctv", axum::routing::post(ctv));
+        .route("/outputs", axum::routing::post(outputs));
     let listener = TcpListener::bind("0.0.0.0:5555").await?;
     axum::serve(listener, app).await?;
     Ok(())
@@ -22,15 +27,6 @@ struct IndexTemplate;
 async fn index() -> IndexTemplate {
     IndexTemplate
 }
-
-#[derive(Deserialize)]
-struct CtvRequest {
-    #[serde(with = "bitcoin::consensus::serde::With::<bitcoin::consensus::serde::Hex>")]
-    txhash: Transaction,
-    input: u32,
-    network: Network,
-}
-
 #[derive(Template)]
 #[template(path = "ctv.html")]
 struct CtvTemplate {
@@ -40,8 +36,41 @@ struct CtvTemplate {
     address: String,
 }
 
-async fn ctv(Form(request): Form<CtvRequest>) -> CtvTemplate {
-    let tmplhash = ctv::ctv(&request.txhash, request.input);
+#[derive(Deserialize)]
+struct OutputsRequest {
+    outputs: String,
+    input: u32,
+    network: Network,
+}
+
+async fn outputs(Form(request): Form<OutputsRequest>) -> CtvTemplate {
+    let outputs: Vec<_> = request
+        .outputs
+        .lines()
+        .map(|line| {
+            let (address, amount) = line.split_once(':').unwrap();
+            let address = Address::from_str(address)
+                .unwrap()
+                .require_network(request.network)
+                .unwrap();
+            let amount = Amount::from_str(amount).unwrap();
+            (address, amount)
+        })
+        .map(|(address, amount)| TxOut {
+            value: amount,
+            script_pubkey: address.script_pubkey(),
+        })
+        .collect();
+    let tx = Transaction {
+        version: Version::ONE,
+        lock_time: LockTime::ZERO,
+        input: vec![TxIn {
+            sequence: Sequence::ZERO,
+            ..Default::default()
+        }],
+        output: outputs,
+    };
+    let tmplhash = ctv::ctv(&tx, request.input);
     let locking_script = ctv::segwit::locking_script(&tmplhash);
     let address = ctv::segwit::locking_address(&locking_script, request.network).to_string();
     CtvTemplate {
