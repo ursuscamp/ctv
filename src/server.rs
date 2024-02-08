8c27303a18df06d2ec082c0b4fdaf46b26f3f2de
Ryan Breen <ryan@breen.xyz>
2024-02-08 13:30:32 -0500

    Functional locking and unlocking, working on documentation.

--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,15 @@
 use std::str::FromStr;
 
 use askama::Template;
-use axum::{Form, Router};
+use axum::Router;
+use axum_extra::extract::Form;
 use bitcoin::{
-    absolute::LockTime, transaction::Version, Address, Amount, Network, Sequence, Transaction,
-    TxIn, TxOut,
+    absolute::LockTime, address::NetworkUnchecked, script::PushBytesBuf, transaction::Version,
+    Address, Amount, Network, OutPoint, Psbt, ScriptBuf, Sequence, Transaction, TxIn, TxOut, Txid,
+    Witness,
 };
 use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
 use tokio::net::TcpListener;
 
 use crate::ctv;
@@ -14,7 +17,8 @@ use crate::ctv;
 pub async fn server() -> anyhow::Result<()> {
     let app = Router::new()
         .route("/", axum::routing::get(index))
-        .route("/outputs", axum::routing::post(outputs));
+        .route("/locking", axum::routing::post(locking))
+        .route("/spending", axum::routing::post(spending));
     let listener = TcpListener::bind("0.0.0.0:5555").await?;
     axum::serve(listener, app).await?;
     Ok(())
@@ -28,12 +32,15 @@ async fn index() -> IndexTemplate {
     IndexTemplate
 }
 #[derive(Template)]
-#[template(path = "ctv.html")]
+#[template(path = "locking.html")]
 struct CtvTemplate {
     ctv: String,
     locking_script: String,
     locking_hex: String,
     address: String,
+    input: u32,
+    addresses: Vec<Address>,
+    amounts: Vec<Amount>,
 }
 
 #[derive(Deserialize)]
@@ -43,21 +50,24 @@ struct OutputsRequest {
     network: Network,
 }
 
-async fn outputs(Form(request): Form<OutputsRequest>) -> CtvTemplate {
-    let outputs: Vec<_> = request
-        .outputs
-        .lines()
-        .map(|line| {
-            let (address, amount) = line.split_once(':').unwrap();
-            let address = Address::from_str(address)
-                .unwrap()
-                .require_network(request.network)
-                .unwrap();
-            let amount = Amount::from_str(amount).unwrap();
-            (address, amount)
-        })
+async fn locking(Form(request): Form<OutputsRequest>) -> CtvTemplate {
+    let mut addresses = Vec::new();
+    let mut amounts = Vec::new();
+    for line in request.outputs.lines() {
+        let (address, amount) = line.split_once(':').unwrap();
+        let address = Address::from_str(address)
+            .unwrap()
+            .require_network(request.network)
+            .unwrap();
+        let amount = Amount::from_str(amount).unwrap();
+        addresses.push(address);
+        amounts.push(amount);
+    }
+    let outputs: Vec<_> = addresses
+        .iter()
+        .zip(amounts.iter())
         .map(|(address, amount)| TxOut {
-            value: amount,
+            value: *amount,
             script_pubkey: address.script_pubkey(),
         })
         .collect();
@@ -78,5 +88,62 @@ async fn outputs(Form(request): Form<OutputsRequest>) -> CtvTemplate {
         locking_script: locking_script.to_string(),
         locking_hex: hex::encode(locking_script.into_bytes()),
         address: address.to_string(),
+        input: request.input,
+        addresses,
+        amounts,
+    }
+}
+
+#[serde_as]
+#[derive(Debug, Deserialize)]
+struct SpendingRequest {
+    ctv: String,
+    input: u32,
+    addresses: Vec<Address<NetworkUnchecked>>,
+    #[serde_as(as = "Vec<DisplayFromStr>")]
+    amounts: Vec<Amount>,
+    txid: Txid,
+}
+
+#[derive(Template)]
+#[template(path = "spending.html")]
+struct SpendingTemplate {
+    tx: String,
+}
+
+async fn spending(Form(request): Form<SpendingRequest>) -> SpendingTemplate {
+    println!("{request:#?}");
+    let ctv = hex::decode(&request.ctv).unwrap();
+    let ctvpb = PushBytesBuf::try_from(ctv.clone()).unwrap();
+    let script_sig = bitcoin::script::Builder::new()
+        .push_slice(ctvpb)
+        .into_script();
+    let output: Vec<TxOut> = request
+        .addresses
+        .iter()
+        .zip(request.amounts.iter())
+        .map(|(address, amount)| TxOut {
+            value: *amount,
+            script_pubkey: address.clone().assume_checked().script_pubkey(),
+        })
+        .collect();
+    let mut witness = Witness::new();
+    witness.push(&ctv::segwit::locking_script(&ctv));
+    let tx = Transaction {
+        version: Version::ONE,
+        lock_time: LockTime::ZERO,
+        input: vec![TxIn {
+            previous_output: OutPoint {
+                txid: request.txid,
+                vout: request.input,
+            },
+            script_sig: Default::default(),
+            sequence: Sequence::ZERO,
+            witness,
+        }],
+        output,
+    };
+    SpendingTemplate {
+        tx: hex::encode(bitcoin::consensus::serialize(&tx)),
     }
 }
